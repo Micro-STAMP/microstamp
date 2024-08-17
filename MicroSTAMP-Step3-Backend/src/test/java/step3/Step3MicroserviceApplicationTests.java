33f88593638cd4271e3da69fcb664bed4dc5d695
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-08-16 22:03:24 -0300

    test: commenting test class

--- MicroSTAMP-Step3-Backend/src/test/java/step3/Step3MicroserviceApplicationTests.java
+++ MicroSTAMP-Step3-Backend/src/test/java/step3/Step3MicroserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package step3;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Step3MicroserviceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package step3;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Step3MicroserviceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
