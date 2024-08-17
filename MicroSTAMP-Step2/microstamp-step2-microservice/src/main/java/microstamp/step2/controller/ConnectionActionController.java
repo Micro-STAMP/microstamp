fc5fea8ef8c07067e462509300ea7cec4c1c5f1f
JoaoHugo <joaohmarinho99@gmail.com>
2024-08-17 01:12:36 -0300

    feat: control action apis

--- MicroSTAMP-Step2/microstamp-step2-microservice/src/main/java/microstamp/step2/controller/ConnectionActionController.java
+++ MicroSTAMP-Step2/microstamp-step2-microservice/src/main/java/microstamp/step2/controller/ConnectionActionController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/connectionActions")
+@RequestMapping("/connection-actions")
 @Tag(name = "ConnectionAction")
 public class ConnectionActionController {
 
