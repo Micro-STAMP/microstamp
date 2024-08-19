173b95cc1d4773f2ba21cc659486f75df178a1f4
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-08-19 12:23:28 -0300

    refact: refactoring ruleService's methods

--- MicroSTAMP-Step3-Backend/src/main/java/step3/dto/mit/rule/RuleCreateDto.java
+++ MicroSTAMP-Step3-Backend/src/main/java/step3/dto/mit/rule/RuleCreateDto.java
@@ -3,20 +3,21 @@ package step3.dto.mit.rule;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import step3.entity.UCAType;
+import step3.entity.mit.UCAType;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public record RuleCreateDto(
         @NotBlank
         String name,
         @NotNull
-        Long control_action_id,
+        UUID control_action_id,
         @NotEmpty
-        List<Long> values_ids,
+        List<UUID> values_ids,
         @NotEmpty
         Set<UCAType> types,
         @NotNull
-        Long hazard_id
+        UUID hazard_id
 ) {}
