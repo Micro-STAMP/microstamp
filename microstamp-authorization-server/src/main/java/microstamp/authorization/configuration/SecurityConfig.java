0b917b1316991339d69905538f5293be8db62e0c
JoaoHugo <joaohmarinho99@gmail.com>
2024-08-19 01:03:31 -0300

    refactor: restructure security and token configuration in authorization server

--- microstamp-authorization-server/src/main/java/microstamp/authorization/configuration/SecurityConfig.java
+++ microstamp-authorization-server/src/main/java/microstamp/authorization/configuration/SecurityConfig.java
@@ -1,92 +1,18 @@
 package microstamp.authorization.configuration;
 
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private static final String[] CORS_WHITELIST = {
-            "http://127.0.0.1:9000",
-            "http://127.0.0.1:9001",
-            "http://127.0.0.1:9002",
-            "http://127.0.0.1:9003",
-            "http://127.0.0.1:9004",
-            "http://127.0.0.1:9191"
-    };
-
-    private static final String[] AUTH_WHITELIST = {
-            "/swagger/**",
-            "/swagger-ui/**",
-            "/v3/**",
-            "/oauth2/token"
-    };
-
-    @Bean
-    @Order(1)
-    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());
-
-        http.exceptionHandling((exceptions) -> exceptions
-                        .defaultAuthenticationEntryPointFor(
-                                new LoginUrlAuthenticationEntryPoint("/login"),
-                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)))
-                        .oauth2ResourceServer((resourceServer) -> resourceServer
-                                .jwt(Customizer.withDefaults()));
-
-        return http.build();
-    }
-
-    @Bean
-    @Order(2)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(AUTH_WHITELIST).permitAll()
-                        .anyRequest().authenticated())
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfig()))
-                .oauth2ResourceServer((resourceServer) -> resourceServer.jwt(Customizer.withDefaults()))
-                .formLogin(Customizer.withDefaults());
-
-        return http.build();
-    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -99,58 +25,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public JWKSource<SecurityContext> jwkSource() {
-        KeyPair keyPair = generateRsaKey();
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        RSAKey rsaKey = new RSAKey.Builder(publicKey)
-                .privateKey(privateKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
-        JWKSet jwkSet = new JWKSet(rsaKey);
-        return new ImmutableJWKSet<>(jwkSet);
-    }
-
-    private static KeyPair generateRsaKey() {
-        KeyPair keyPair;
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            keyPair = keyPairGenerator.generateKeyPair();
-        }
-        catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return keyPair;
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
-    }
-
-    @Bean
-    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
-        return (context) -> {
-            String username = context.getPrincipal().getName();
-            if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
-
-                /*if(context.getAuthorizationGrantType() == AuthorizationGrantType.CLIENT_CREDENTIALS) {
-                    username = "admin";
-                    context.getClaims().audience(List.of("fbefbe1f-933d-4012-b730-16db007cd13e"));
-                }
-*/
-                UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService()
-                        .loadUserByUsername(username);
-
-                context.getClaims().claims((claims) -> {
-                    claims.put("userId", userDetails.getUserId());
-                });
-            }
-        };
-    }
-
-    @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -159,17 +33,4 @@ public class SecurityConfig {
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
     }
-
-    private UrlBasedCorsConfigurationSource corsConfig() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList(CORS_WHITELIST));
-        corsConfig.setAllowedMethods(Collections.singletonList("*"));
-        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
-        corsConfig.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration("/**", corsConfig);
-        return configSource;
-    }
-
 }
--- microstamp-authorization-server/src/main/resources/db/migration/V08__INSERT_ADMIN_CLIENT.sql
+++ microstamp-authorization-server/src/main/resources/db/migration/V08__INSERT_ADMIN_CLIENT.sql
@@ -1,26 +1,8 @@
 INSERT INTO oauth2_registered_client
 (id, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_name, client_authentication_methods, authorization_grant_types, redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
 VALUES(uuid(),
-       'fbefbe1f-933d-4012-b730-16db007cd13e',
-	   now(),
-	   '$2a$10$KTxMbNUqXonlI98nVxNvdO9ONGFZ8dCPBQJDSlWBMhGih8lfHsZGy',
-	   NULL,
-	   'microstamp',
-	   'client_secret_basic',
-	   'refresh_token,client_credentials,authorization_code',
-	   'http://127.0.0.1:9000/login/oauth2/code/client-server-microstamp',
-	   'http://127.0.0.1:9000/',
-	   'openid,profile',
-	   '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":true}',
-	   '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}');
-
-
-
-INSERT INTO oauth2_registered_client
-(id, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_name, client_authentication_methods, authorization_grant_types, redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
-VALUES(uuid(),
         'admin',
-        '2024-08-07 02:19:12',
+        now(),
         '$2a$10$FVRj8e83jjoGnZAY55Vol.JSG65F1rpsu0Sq.0RaX41LMnwld0PKa',
         NULL,
         'admin-client',
