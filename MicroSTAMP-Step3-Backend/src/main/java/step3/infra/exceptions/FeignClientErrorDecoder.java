0a0d0348e1d16c718a758965a17d9f6b29a45b62
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-08-26 16:17:25 -0300

    feat: refactoring error handling

--- MicroSTAMP-Step3-Backend/src/main/java/step3/infra/exceptions/FeignClientErrorDecoder.java
+++ MicroSTAMP-Step3-Backend/src/main/java/step3/infra/exceptions/FeignClientErrorDecoder.java
@@ -19,24 +19,23 @@ public class FeignClientErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
+//        log.info("Error Decoder: {}, {}", methodKey, response);
         Response.Body responseBody = response.body();
-        HttpStatus responseStatus = HttpStatus.valueOf(response.status());
         FeignErrorResponse message = null;
+
         try(InputStream bodyIs =  responseBody.asInputStream()) {
             ObjectMapper mapper = new ObjectMapper();
             message = mapper.readValue(bodyIs, FeignErrorResponse.class);
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
         }
-
         String messageError = message.errors().get(0).message();
-        if (responseStatus.is4xxClientError()) {
-            return new EntityNotFoundException(messageError != null ? messageError : "Bad Request.");
-        } else if (responseStatus.is5xxServerError()) {
-            return new RuntimeException(messageError != null ? messageError : "Unexpected Error.");
-        }
-        log.info("Error Decoder: {}, {}", methodKey, response);
 
-        return new Exception("Unknown exception occurred.");
+        if (response.status() == 404) {
+            throw new EntityNotFoundException(messageError != null ? messageError : "Resource not found.");
+        }
+
+        throw new RuntimeException(messageError != null ? messageError : "Unexpected Error.");
+
     }
 }
