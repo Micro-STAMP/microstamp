34dd6694ebb98f6332358b790b4d5a893071c3a2
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-09-10 21:20:59 -0300

    feat: add connection dto

---
+++ microstamp-step3/src/main/java/step3/dto/mit/step2/ConnectionActionType.java
@@ -0,0 +1,10 @@
+package step3.dto.mit.step2;
+
+public enum ConnectionActionType {
+    CONTROL_ACTION,
+    FEEDBACK,
+    COMMUNICATION_CHANNEL,
+    PROCESS_INPUT,
+    PROCESS_OUTPUT,
+    DISTURBANCE
+}
