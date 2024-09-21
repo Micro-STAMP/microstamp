02d8a88e5517485f6ea049beabccbba7dc24c9b0
JoaoHugo <joaohmarinho99@gmail.com>
2024-09-21 16:42:20 -0300

    fix: temporarily remove consent page requirement for MicroSTAMP clients

---
+++ microstamp-authorization-server/src/main/resources/db/migration/V11__UPDATE_CLIENTS_REMOVE_AUTH_CONSENT.sql
@@ -0,0 +1,19 @@
+UPDATE oauth2_registered_client
+	SET client_settings='{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}'
+	WHERE id='9a52b514-6e15-11ef-83fb-e89c25de6c65';
+
+UPDATE oauth2_registered_client
+	SET client_settings='{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}'
+	WHERE id='9a52c0f0-6e15-11ef-83fb-e89c25de6c65';
+
+UPDATE oauth2_registered_client
+	SET client_settings='{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}'
+	WHERE id='9a52cc71-6e15-11ef-83fb-e89c25de6c65';
+
+UPDATE oauth2_registered_client
+	SET client_settings='{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}'
+	WHERE id='9a52d8c6-6e15-11ef-83fb-e89c25de6c65';
+
+UPDATE oauth2_registered_client
+	SET client_settings='{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}'
+	WHERE id='9a52e45a-6e15-11ef-83fb-e89c25de6c65';
