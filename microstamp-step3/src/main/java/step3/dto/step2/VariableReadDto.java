2d8f3b8770c82e87146fdce3b60f0a921569963f
Fellipe Rey <fellipe_guilherme@hotmail.com>
2024-09-22 19:47:05 -0300

    refactor: Adding builder to DTOs

--- microstamp-step3/src/main/java/step3/dto/step2/VariableReadDto.java
+++ microstamp-step3/src/main/java/step3/dto/step2/VariableReadDto.java
@@ -1,8 +1,11 @@
 package step3.dto.step2;
 
+import lombok.Builder;
+
 import java.util.List;
 import java.util.UUID;
 
+@Builder
 public record VariableReadDto(
         UUID id,
         String name,
