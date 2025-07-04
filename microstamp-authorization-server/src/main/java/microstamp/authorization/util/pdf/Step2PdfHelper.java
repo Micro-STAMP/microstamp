703d8dbf0a9e265579a6700dbc0c5f2aebff3fcc
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2025-07-03 22:35:33 -0300

    feat: add hyperlinks in pdf of step2

--- microstamp-authorization-server/src/main/java/microstamp/authorization/util/pdf/Step2PdfHelper.java
+++ microstamp-authorization-server/src/main/java/microstamp/authorization/util/pdf/Step2PdfHelper.java
@@ -3,6 +3,7 @@ package microstamp.authorization.util.pdf;
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.ListItem;
 import com.itextpdf.layout.element.Paragraph;
 import microstamp.authorization.dto.step2.*;
 
@@ -15,39 +16,42 @@ public class Step2PdfHelper {
         com.itextpdf.layout.element.List componentsList = new com.itextpdf.layout.element.List();
 
         for(ComponentReadDto component : components) {
-            StringBuilder componentRow = new StringBuilder("[" + component.getType() + "] ");
-            componentRow.append("[").append(component.getCode()).append("] ");
-            componentRow.append(component.getName());
+            Paragraph componentParagraph = new Paragraph("[" + component.getType() + "] [" + component.getCode() + "] " + component.getName());
+            componentParagraph.setDestination(component.getCode());
 
-            if(component.getFather() != null)
-                componentRow.append(" [").append(component.getFather().getCode()).append("] ");
+            if (component.getFather() != null) {
+                componentParagraph.add(" [" + component.getFather().getCode() + "] ");
+                componentParagraph.setDestination(component.getFather().getCode());
+            }
 
             if(!component.getResponsibilities().isEmpty()) {
-                componentRow.append("\n[Responsibilities]:");
+                componentParagraph.add("\n[Responsibilities]:");
 
                 for(ResponsibilityReadDto responsibility : component.getResponsibilities()){
-                    componentRow.append("\n").append("\u200B    ")
-                            .append("-[").append(responsibility.getCode()).append("] ")
-                            .append(responsibility.getResponsibility());
+                    componentParagraph.add("\n\u200B    -[" + responsibility.getCode() + "] " + responsibility.getResponsibility());
+                    componentParagraph.setDestination(responsibility.getCode());
                 }
             }
 
-            if(!component.getVariables().isEmpty()) {
-                componentRow.append("\n[Variables]:");
+            if (!component.getVariables().isEmpty()) {
+                componentParagraph.add("\n[Variables]:");
 
                 for(VariableReadDto variable : component.getVariables()) {
-                    componentRow.append("\n").append("\u200B    ")
-                            .append("-[").append(variable.getCode()).append("] ").append(variable.getName());
+                    componentParagraph.add("\n\u200B    -[" + variable.getCode() + "] " + variable.getName());
+                    componentParagraph.setDestination(variable.getCode());
 
                     if(!variable.getStates().isEmpty()) {
-                        for(StateReadDto state : variable.getStates())
-                            componentRow.append("\n").append("\u200B        ")
-                                    .append("-[").append(state.getCode()).append("] ").append(state.getName());
+                        for(StateReadDto state : variable.getStates()) {
+                            componentParagraph.add("\n\u200B        -[" + state.getCode() + "] " + state.getName());
+                            componentParagraph.setDestination(state.getCode());
+                        }
                     }
                 }
             }
 
-            componentsList.add(componentRow.toString());
+            ListItem componentItem = new ListItem();
+            componentItem.add(componentParagraph);
+            componentsList.add(componentItem);
         }
 
         document.add(new Paragraph("Components")
@@ -60,19 +64,19 @@ public class Step2PdfHelper {
         com.itextpdf.layout.element.List connectionsList = new com.itextpdf.layout.element.List();
 
         for(ConnectionReadDto connection : connections) {
-            StringBuilder connectionRow = new StringBuilder("[" + connection.getCode() + "] ");
-            connectionRow.append(connection.getSource().getName())
-                    .append(" --> ")
-                    .append(connection.getTarget().getName());
+            Paragraph connectionParagraph = new Paragraph("[" + connection.getCode() + "] ");
+            connectionParagraph.add(connection.getSource().getName() + " --> " + connection.getTarget().getName());
+            connectionParagraph.setDestination(connection.getCode());
 
-            for(InteractionReadDto interaction : connection.getInteractions()){
-                connectionRow.append("\n").append("\u200B    ")
-                        .append("-[").append(interaction.getInteractionType().getFormattedName()).append("] ")
-                        .append("[").append(interaction.getCode()).append("] ")
-                        .append(interaction.getName());
+            for (InteractionReadDto interaction : connection.getInteractions()) {
+                connectionParagraph.add("\n\u200B    -[" + interaction.getInteractionType().getFormattedName() + "] "
+                        + "[" + interaction.getCode() + "] " + interaction.getName());
+                connectionParagraph.setDestination(interaction.getCode());
             }
 
-            connectionsList.add(connectionRow.toString());
+            ListItem connectionItem = new ListItem();
+            connectionItem.add(connectionParagraph);
+            connectionsList.add(connectionItem);
         }
 
 
