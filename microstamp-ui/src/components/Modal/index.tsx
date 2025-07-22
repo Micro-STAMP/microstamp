156f716e360252998323eecadf268195c06b8ad4
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2025-07-21 23:27:08 -0300

    feat: add modal select options

--- microstamp-ui/src/components/Modal/index.tsx
+++ microstamp-ui/src/components/Modal/index.tsx
@@ -3,11 +3,13 @@ import ModalInteractions from "./ModalInteractions";
 import ModalLoading from "./ModalLoading";
 import ModalPDFPreview from "./ModalPDFPreview";
 import ModalSelectControlAction from "./ModalSelectControlAction";
+import ModalSelectOptions from "./ModalSelectOptions";
 
 export {
 	ModalConfirm,
 	ModalInteractions,
 	ModalLoading,
 	ModalPDFPreview,
-	ModalSelectControlAction
+	ModalSelectControlAction,
+	ModalSelectOptions
 };
